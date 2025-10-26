a3e4cde266ea526779989689880e0686bd72f172
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2025-10-26 12:48:02 +0000

    Moved everything into a proper test directory and a test file.

---
+++ test/run-test.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageChops
+import math, operator, os
+
+def rmsdiff(im1, im2):
+    "Calculate the root-mean-square difference between two images"
+    h = ImageChops.difference(im1, im2).histogram()
+    sq = (value*((idx%256)**2) for idx, value in enumerate(h))
+    sum_of_squares = sum(sq)
+    rms = math.sqrt(sum_of_squares / (im1.size[0] * im1.size[1]))
+    return rms
+
+im1 = Image.open(os.environ('FILE')).convert("RGB")
+im2 = Image.open(os.environ('EXPECTEDFILE')).convert("RGB")
+
+tolerance = 10  # Adjust RMS tolerance
+diff = rmsdiff(im1, im2)
+print(f"RMS difference: {diff}")
+if diff > tolerance:
+    raise Exception(f"Image is too different from expected (tolerance {tolerance})")
