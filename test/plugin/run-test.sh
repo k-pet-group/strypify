1654d6846ba1cb17976bbfe58759f278b3bc4f3c
Neil Brown <neil@twistedsquare.com>
2025-10-30 12:16:44 +0000

    Trying to debug PATH issue.

--- test/plugin/run-test.sh
+++ test/plugin/run-test.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+echo $PATH
+
 asciidoctor -I../../asciidoctor -r strypify-plugin example1.adoc --trace
 
 diff -u expected-example1.html example1.html || { echo "Files differ!"; exit 1; }
