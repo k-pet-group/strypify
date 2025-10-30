d084011476be59904c46dffd6211015c1be93a36
Neil Brown <neil@twistedsquare.com>
2025-10-30 12:12:00 +0000

    Set the PATH from the Github Actions side rather than in the test script itself

--- test/plugin/run-test.sh
+++ test/plugin/run-test.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-shopt -s nullglob
-PATH="$(printf "%s:" ../../artifact/Strypify*/ ../../Strypify*/):$PATH" asciidoctor -I../../asciidoctor -r strypify-plugin example1.adoc --trace
+asciidoctor -I../../asciidoctor -r strypify-plugin example1.adoc --trace
 
 diff -u expected-example1.html example1.html || { echo "Files differ!"; exit 1; }
