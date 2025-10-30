1ed0bead60a9ebaafb92cd5561c046d35b03ba6d
Neil Brown <neil@twistedsquare.com>
2025-10-30 10:55:49 +0000

    Adjusted test path to work in CI

--- test/plugin/run-test.sh
+++ test/plugin/run-test.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-PATH=../../Strypify*/:$PATH asciidoctor -I../../asciidoctor -r strypify-plugin example1.adoc --trace
+PATH=../../artifact/Strypify*/:../../Strypify*/:$PATH asciidoctor -I../../asciidoctor -r strypify-plugin example1.adoc --trace
 
 diff -u expected-example1.html example1.html || { echo "Files differ!"; exit 1; }
