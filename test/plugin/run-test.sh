5372d3b3854e3fbac1fedc3d7b6a259263bc03ab
Neil Brown <neil@twistedsquare.com>
2025-10-30 12:07:54 +0000

    Fixed PATH calculation in test script

--- test/plugin/run-test.sh
+++ test/plugin/run-test.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-PATH=../../artifact/Strypify*/:../../Strypify*/:$PATH asciidoctor -I../../asciidoctor -r strypify-plugin example1.adoc --trace
+shopt -s nullglob
+PATH="$(printf "%s:" ../../artifact/Strypify*/ ../../Strypify*/):$PATH" asciidoctor -I../../asciidoctor -r strypify-plugin example1.adoc --trace
 
 diff -u expected-example1.html example1.html || { echo "Files differ!"; exit 1; }
